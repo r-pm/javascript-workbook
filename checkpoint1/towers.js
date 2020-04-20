c3fc18117e23e491be93dd6ea7ac1dfe4c307c43
r-pm <rociopenamartinez@gmail.com>
2020-04-20 15:09:18 -0500

    this is where i was at when i realized i was both trying to create a terminal app game & have the game solve itself

--- checkpoint1/towers.js
+++ checkpoint1/towers.js
@@ -7,11 +7,12 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let stacks = {
-  a: [4, 3, 2, 1],
+let stacks = {   ///need to connect to html stacks
+  a: [3, 2, 1],  
   b: [],
-  c: []
+  c: [],
 };
+
 
 const printStacks = () => {
   console.log("a: " + stacks.a);
@@ -19,33 +20,56 @@ const printStacks = () => {
   console.log("c: " + stacks.c);
 }
 
-const movePiece = (startStack, endStack) => {
-  // Your code here
+let start = stacks.a; //tower we start moving from  
+let end = stacks.c; //tower we end moving to 
+let nDisks = stacks[element].length 
+// nDisk should count total number of discs [#,#,#] in the stack selected?/being worked with 
 
+const movePiece = (nDisks, start, end) => {
+  let movingDisk = start.pop([nDisks-1]); //pop last (nDisks-1) value of the start tower
+  let destination = end.pop([nDisks-1]); //pop the last value of the end tower
+ //what if it's empty tho?
+  if (movingDisk < destination) {
+  end.push(movingDisk);
+}
+  else { print = "invalid move"} //connect this to an html elem
 }
 
-const isLegal = (startStack, endStack) => {
-  // move rules, sz rules 
-  //returns true if all good 
-
+const isLegal = (start, end) => {
+  //if number want to move smaller than last number of stack you want to move to 
+  // 
 }
 
 const checkForWin = () => {
-  // return true if win = 
-  //if all pieces moved to c, in order 
-
+  if ( stacks.c === [3, 2, 1]){
+    print = "Finished!";
+    return true;
+  }
+//...this one seems easy enough
 }
 
-const towersOfHanoi = (startStack, endStack) => {
-  let fromStack = stacks[startStack]; 
-  endStack = 
+
+const play = (nDisks, start , via, end) => {
+
+  if (nDisks == 1) {
+
+  }
+
+ELSE
+play(nDisks - 1, start, end, via) // 1 to c , 
+move disk from start to end // Step 2
+play(disk - 1, via, start, end) // 2 to b 
+UNTIL
+IF nDisks is equal 1, THEN
+move disk from start to end
+
 }
 
 const getPrompt = () => {
   printStacks();
-  rl.question('start stack: ', (startStack) => {
-    rl.question('end stack: ', (endStack) => {
-      towersOfHanoi(startStack, endStack);
+  rl.question('start stack: ', (start) => {
+    rl.question('end stack: ', (end) => {
+      playTowers(start, end);
       getPrompt();
     });
   });
