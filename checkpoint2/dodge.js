5d9f9426e1adc7d1f05d6fff992d096aa1717cdb
r-pm <rociopenamartinez@gmail.com>
2020-05-12 22:01:01 -0500

    pseudo code draft for js

---
+++ checkpoint2/dodge.js
@@ -0,0 +1,61 @@
+'use strict'
+let people = [];
+let players = [];
+///  input: fetch (random user)
+// window.onload = 
+function getInput() {
+  fetch('http://example.com/https://randomuser.me/api/?results=6')
+    .then(response => response.json())
+    console.log(response)
+    .then(data => console.log(data));
+    
+};
+///   create a new person class 
+///   add to pool of people array
+function addPeople(data){
+  class newPerson {
+    constructor(){
+
+    }
+  }
+  people.push(this.newPerson);
+};
+
+///   display: pool of people with onclick
+///   include button to approve
+function listPeople(){
+  document.getElementById('people')
+  //appendChild ea people item
+  //appendChild approval button to ea item
+};
+
+///   on click, pass person to class for newPlayer
+//add event handler to approval button click
+function approvePlayer(){
+  class newPlayer {
+    constructor(){
+
+    }
+    players.push(this.newPlayer);
+
+  }
+};
+
+///  display approved players array
+function displayPlayers(){
+  //appendChild players[]
+  document.getElementById('players').appendChild 
+  //remove from people[]?
+  //create team assignment button for each 
+
+};
+
+///  onclick pass person to class Teammate with selected team attribute 
+function assignTeam(){
+  //event handler for team assignment button
+  /*push to team arrays
+    class teammate{
+    team name:
+  }* extend player class with team name
+  */
+};
